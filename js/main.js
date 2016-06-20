260c83a6392458cce153529eaa2153ce5979c0b9
jScheq <t.bitw.fl@yandex.ru>
2016-06-21 01:46:22 +0500

    Helpers + best practice namespace

--- js/main.js
+++ js/main.js
@@ -1,108 +1,119 @@
-// Пространство имен для приложения
-var Person = {
-    Collection : {},
-    Model : {},
-    View : {}
-};
+// Замыкание
+(function() {
 
-// Такое приходит с сервера
-var people = [
-    {
-        name: 'Петр',
-        age: 27,
-        job: 'Менеджер',
-    },
-    {
-        name: 'Игорь',
-        age: 22,
-        job: 'Продавец',
-    },
-    {
-        name: 'Альберт',
-        job: 'Кассир',
-    },
-];
+    // Пространство имен для приложения
+    window.App = {
+        Collections : {},
+        Models : {},
+        Views : {},
+        Helpers: {}
+    };
 
-/**
- * Создание модели объекта
- */
-Person.Model.Item = Backbone.Model.extend({
+    // Хелпер шаблона
+    App.Helpers.template = function(id) {
+        return _.template($('#' + id).html());
+    }
 
-    // Значение инстанса по умолчанию
-    defaults: {
-        name: 'Dima',
-        age: 25,
-        job: 'Web Developer'
-    },
+    // Такое приходит с сервера
+    var people = [
+        {
+            name: 'Петр',
+            age: 27,
+            job: 'Менеджер',
+        },
+        {
+            name: 'Игорь',
+            age: 22,
+            job: 'Продавец',
+        },
+        {
+            name: 'Альберт',
+            job: 'Кассир',
+        },
+    ];
 
-    // Валидация возраста
-    validate: function(attrs) {
-        if (attrs.age <= 0) {
-            return 'Ошибка при установке значения атрибута age: ожидается положительное число';
+    /**
+     * Создание модели объекта
+     */
+    App.Models.Person = Backbone.Model.extend({
+
+        // Значение инстанса по умолчанию
+        defaults: {
+            name: 'Dima',
+            age: 25,
+            job: 'Web Developer'
+        },
+
+        // Валидация возраста
+        validate: function(attrs) {
+            if (attrs.age <= 0) {
+                return 'Ошибка при установке значения атрибута age: ожидается положительное число';
+            }
+        },
+
+        // Метод объекта
+        walk: function() {
+            return this.get('name') + ' is walking';
         }
-    },
+    });
 
-    // Метод объекта
-    walk: function() {
-        return this.get('name') + ' is walking';
-    }
-});
+    /**
+     * Список людей
+     */
+    App.Collections.People = Backbone.Collection.extend({
+        // передаем инициализирующую модель с которой ассоциируются будущие модели
+        model: App.Models.Person,
+    });
 
-/**
- * Список людей
- */
-Person.Collection.Items = Backbone.Collection.extend({
-    // передаем инициализирующую модель с которой ассоциируются будущие модели
-    model: Person.Model.Item,
-});
+    /**
+     * Создаем вид одного человека
+     */
+    App.Views.Person = Backbone.View.extend({
 
-/**
- * Создаем вид одного человека
- */
-Person.View.Item = Backbone.View.extend({
+        // Срабатывает в момент создания экземпляра класса
+        initialize: function() {
+            // бест-практиз рендер на этапе инициализации
+            this.render();
+        },
+        tagName: 'li',
 
-    // Срабатывает в момент создания экземпляра класса
-    initialize: function() {
-        // бест-практиз рендер на этапе инициализации
-        this.render();
-    },
-    tagName: 'li',
+        // Внешний шаблон
+        template: App.Helpers.template('personal-id'),
 
-    // Внешний шаблон
-    template: _.template($('#personal-id').html()),
+        // Наполнение документа HTML кодом
+        render: function() {
+            this.$el.html(this.template(this.model.toJSON()));
+            return this;
+        }
+    });
 
-    // Наполнение документа HTML кодом
-    render: function() {
-        this.$el.html(this.template(this.model.toJSON()));
-        return this;
-    }
-});
+    /**
+     * Вид списка людей
+     */
+    App.Views.People = Backbone.View.extend({
+        tagName: 'ul',
+        className: 'people-list',
+        initialize: function() {
 
-/**
- * Вид списка людей
- */
-Person.View.Items = Backbone.View.extend({
-    tagName: 'ul',
-    className: 'people-list',
-    initialize: function() {
+        },
+        render: function () {
+            this.collection.each(function(person) {
+                var personView = new App.Views.Person({model: person});
+                this.$el.append(personView.el);
+            }, this);
 
-    },
-    render: function () {
-        this.collection.each(function(person) {
-            var personView = new Person.View.Item({model: person});
-            this.$el.append(personView.el);
-        }, this);
+            return this;
+        }
+    });
 
-        return this;
-    }
-});
+    var person = new App.Models.Person;
 
-var person = new Person.Model.Item;
+    // Заполняем коллекцию массивом объектов
+    var peopleCollection = new App.Collections.People(people);
 
-// Заполняем коллекцию массивом объектов
-var peopleCollection = new Person.Collection.Items(people);
+    // Передаем коллекцию peopleCollection в вид списка людей PeopleView
+    var peopleView = new App.Views.People({collection: peopleCollection});
 
-// Передаем коллекцию peopleCollection в вид списка людей PeopleView
-var peopleView = new Person.View.Items({collection: peopleCollection});
+    $(document.body).append(peopleView.render().el);
 
-$(document.body).append(peopleView.render().el);
+}());
